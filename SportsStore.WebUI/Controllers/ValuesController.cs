3567cfbd5039d7ff8b2151849af71f370246fb13
Jake Ryu <jake.ryu@hotmail.co.uk>
2014-06-12 17:37:06 +0100

    api added

---
+++ SportsStore.WebUI/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using SportsStore.Domain.Concrete;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public HttpResponseMessage Get(int id)
+        {
+            EFProductRepository repo = new EFProductRepository();
+
+            var prod = repo.Products.SingleOrDefault(p => p.ProductID == id);
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            Stream strm = new MemoryStream(prod.ImageData);
+            response.Content = new StreamContent(strm);
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+            response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+            response.Content.Headers.ContentDisposition.FileName = "hyundai.apk";
+
+            repo = null;
+
+            return response;
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -15,8 +15,8 @@ namespace SportsStore.WebUI
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-
             ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
